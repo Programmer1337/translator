ae387bbfc9a9ca3acdecd2cf4682e3dca366a15d
Igor <igoryashka2001@gmail.com>
2021-09-12 23:44:25 +0300

    a lot of changes

--- content.js
+++ content.js
@@ -1,17 +1,174 @@
+// const changedElems = []
+// let text
+// let toLang = 'en'
+//
+// chrome.storage.sync.get(['lang'], function (result) {
+//     if (!result.lang) {
+//         chrome.storage.sync.set({lang: 'en'})
+//         toLang = 'en'
+//     } else {
+//         toLang = result.lang
+//     }
+// });
+//
+// document.addEventListener('mousedown', () => {
+//     changedElems.forEach(value => {
+//         value.elem.nodeValue = value.defaultValue
+//         value.elem.parentElement.normalize()
+//     })
+//     while (changedElems.length)
+//         changedElems.pop()
+//     if ((document.getSelection())&&(!document.getSelection().getRangeAt(0).collapsed))
+//         document.getSelection().removeRange(document.getSelection().getRangeAt(0))
+// }, false)
+//
+//
+// chrome.runtime.onMessage.addListener(
+//     function (request, sender, sendResponse) {
+//         if (request.message === "get_lang")
+//             sendResponse({lang: toLang});
+//         if (request.message === "set_lang") {
+//             toLang = request.elem
+//         }
+//     });
+//
+//
+// const sendReq = (text = 'Hello World', elem) => {
+//     const data = JSON.stringify([
+//         {
+//             "Text": text
+//         }
+//     ]);
+//
+//     const xhr = new XMLHttpRequest();
+//     xhr.withCredentials = true;
+//
+//     xhr.addEventListener("readystatechange", function () {
+//
+//         if ((this.readyState === this.DONE) && (!changedElems.every(value => value.elem!==elem))) {
+//             document.body.style.cursor = 'default'
+//             const translatedText = JSON.parse(this.response)[0].translations[0].text
+//             elem.nodeValue = elem.nodeValue.replace(text, translatedText)
+//         }
+//     });
+//
+//     xhr.open("POST", `https://microsoft-translator-text.p.rapidapi.com/translate?api-version=3.0&to=${toLang}&textType=plain&profanityAction=NoAction`);
+//     xhr.setRequestHeader("content-type", "application/json");
+//     xhr.setRequestHeader("x-rapidapi-host", "microsoft-translator-text.p.rapidapi.com");
+//     xhr.setRequestHeader("x-rapidapi-key", "c26b23dd8fmshdd3a251db7ca47fp1b0f2cjsnd350dd6bac9e");
+//
+//     xhr.send(data);
+// }
+//
+//
+// document.addEventListener("mouseup", () => {
+//     text = document.getSelection().toString()
+//     const selection = document.getSelection()
+//     const range = selection.getRangeAt(0)
+//
+//     if (!range.collapsed) {
+//         document.body.style.cursor = 'progress'
+//         const newStartNode = document.createElement("b");
+//         const newEndNode = document.createElement("e");
+//         const newRange = range.cloneRange()
+//         newRange.insertNode(newStartNode)
+//         newRange.setStart(range.endContainer, range.endOffset)
+//         newRange.insertNode(newEndNode)
+//
+//         const afterRange = new Range()
+//         afterRange.setStart(newStartNode, 0)
+//         afterRange.setEnd(newEndNode, 0)
+//         let elem = afterRange.startContainer
+//         for (let i = 0; i < 1350; i++) {
+//             if (elem.firstChild) {
+//                 elem = elem.firstChild
+//             } else if (elem.nextSibling) {
+//                 elem = elem.nextSibling
+//             } else if (elem.parentNode) {
+//                 while (!elem.nextSibling) {
+//                     elem = elem.parentNode
+//                 }
+//                 elem = elem.nextSibling
+//             }
+//
+//             if (elem.nodeType === 3) {
+//                 const newElem = elem
+//                 translate(newElem.nodeValue, newElem)
+//             }
+//             if ((elem instanceof Element) && (elem.tagName === 'E'))
+//                 break
+//         }
+//
+//         document.getSelection().removeRange(range)
+//         const newR = new Range()
+//         newR.setStart(newStartNode, 0)
+//         newR.setEnd(newEndNode, 0)
+//         document.getSelection().addRange(newR)
+//         newStartNode.remove()
+//         newEndNode.remove()
+//     }
+//
+//     function translate(text, elem) {
+//
+//         const filteredText = text.replace(/\s+/g, ' ').trim().split('').filter(value => (value !== '\n')).join('').split(' ').filter(value => (value !== ' ') && (value !== "\n") && (value !== '')).join(' ')
+//         if (filteredText.length)
+//         if ((/[a-zA-Z]/g.test(filteredText)) || (/[а-яА-Я]/g.test(filteredText))) {
+//             changedElems.push({
+//                 elem: elem,
+//                 defaultValue: elem.nodeValue
+//             })
+//             sendReq(filteredText, elem)
+//
+//         }
+//         return text
+//     }
+// })
+//
+//
+//
+
+
 const changedElems = []
 let text
 let toLang = 'en'
+let requestsPending = 0
+let isOn
+
+const synchronizeData=()=>{
+    chrome.storage.sync.get(['lang'], function (result) {
+        console.log('result is ', JSON.stringify(result))
+        if (!result.lang) {
+            chrome.storage.sync.set({lang: 'en'})
+            toLang = 'en'
+        } else {
+            toLang = result.lang
+        }
+    });
+
+    chrome.storage.sync.get(['state'], function (result) {
+        if (result.state === 'off') {
+            isOn = false
+        } else {
+            isOn = true
+            chrome.storage.sync.set({state: 'on'});
+
+        }
+        console.log('сейчас ', isOn)
+    });
 
 
-chrome.storage.sync.get(['lang'], function (result) {
-    console.log('result is ',JSON.stringify( result))
-    if (!result.lang) {
-        chrome.storage.sync.set({lang: 'en'})
-        toLang = 'en'
-    } else {
-        toLang = result.lang
-    }
-});
+}
+
+
+
+
+document.addEventListener('mousedown',()=>{
+    synchronizeData()
+},false)
+
+
+
+
 
 document.addEventListener('mousedown', () => {
     changedElems.forEach(value => {
@@ -20,7 +177,7 @@ document.addEventListener('mousedown', () => {
     })
     while (changedElems.length)
         changedElems.pop()
-    if (!document.getSelection().getRangeAt(0).collapsed)
+    if ((document.getSelection()) && (!document.getSelection().getRangeAt(0).collapsed))
         document.getSelection().removeRange(document.getSelection().getRangeAt(0))
 }, false)
 
@@ -33,11 +190,14 @@ chrome.runtime.onMessage.addListener(
             console.log('lang changed to', request.elem);
             toLang = request.elem
         }
+        if (request.message === "set_state") {
+            console.log('state changed to', request.state);
+            isOn =request.state
+        }
     });
 
 
 const sendReq = (text = 'Hello World', elem) => {
-    const temp = elem
     const data = JSON.stringify([
         {
             "Text": text
@@ -48,12 +208,17 @@ const sendReq = (text = 'Hello World', elem) => {
     xhr.withCredentials = true;
 
     xhr.addEventListener("readystatechange", function () {
-        if ((this.readyState === this.DONE) && (changedElems.length)) {
-            console.log(this)
-            const translatedText = JSON.parse(this.response)[0].translations[0].text
-            console.log("Исходный текст:", text, "Полученный:", translatedText)
-            console.log("Вставить это в элемент", elem, " вместо ", elem.nodeValue)
-            temp.nodeValue = elem.nodeValue.replace(text, translatedText)
+        if (this.readyState === this.DONE) {
+            requestsPending--
+            console.log('reqs left: ', requestsPending)
+            if (!requestsPending)
+                document.body.style.cursor = 'default'
+            if (changedElems.length) {
+                const translatedText = JSON.parse(this.response)[0].translations[0].text
+                console.log("Исходный текст:", text, "Полученный:", translatedText)
+                console.log("Вставить это в элемент", elem, " вместо ", elem.nodeValue)
+                elem.nodeValue = elem.nodeValue.replace(/\s+/g, ' ').replace(text, translatedText)
+            }
         }
     });
 
@@ -65,13 +230,16 @@ const sendReq = (text = 'Hello World', elem) => {
     xhr.send(data);
 }
 
+synchronizeData()
 
 document.addEventListener("mouseup", event => {
     text = document.getSelection().toString()
     const selection = document.getSelection()
     const range = selection.getRangeAt(0)
 
-    if (!range.collapsed) {
+    if (!range.collapsed && isOn) {
+        document.body.style.cursor = 'progress'
+
         const newStartNode = document.createElement("b");
         const newEndNode = document.createElement("e");
         const newRange = range.cloneRange()
@@ -118,7 +286,7 @@ document.addEventListener("mouseup", event => {
 
     function translate(text, elem) {
 
-        const filteredText = text.split('').filter(value => (value !== '\n')).join('').split(' ').filter(value => (value !== ' ') && (value !== "\n") && (value !== '')).join(' ')
+        const filteredText = text.replace(/\s+/g, ' ').trim().split('').filter(value => (value !== '\n')).join('').split(' ').filter(value => (value !== ' ') && (value !== "\n") && (value !== '')).join(' ')
         if (filteredText.length)
             console.log('перевести надо будет ', filteredText, 'в элементе ', elem, elem.nodeValue.includes(filteredText))
         if ((/[a-zA-Z]/g.test(filteredText)) || (/[а-яА-Я]/g.test(filteredText))) {
@@ -128,9 +296,9 @@ document.addEventListener("mouseup", event => {
             })
             console.log('Changed list:', changedElems)
             sendReq(filteredText, elem)
+            requestsPending++
 
         }
         return text
     }
 })
-
--- popup_script.js
+++ popup_script.js
@@ -1,20 +1,58 @@
 document.addEventListener('DOMContentLoaded', function () {
     const selector = document.getElementById("selector")
-    const langs = ['ru', 'en', 'fr', 'de', 'uk', 'it']
+    const button = document.getElementsByClassName("button")[0]
+    let isOn
+
+    const changeView = (state) => {
+        if (state) {
+            button.setAttribute('src', './turn_on_button_red.svg')
+            document.body.style.backgroundImage = "linear-gradient(0deg, rgba(80,174,244,1) 0%, rgba(255,252,252,1) 50%, rgba(80,174,244,1) 100%)"
+            selector.disabled = false
+        } else {
+            button.setAttribute('src', './turn_on_btn.svg')
+            document.body.style.backgroundImage = "linear-gradient(0deg, rgba(30,66,92,1) 0%, rgba(75,74,74,1) 50%, rgba(27,58,82,1) 100%)"
+            selector.disabled = true
+        }
+
+    }
+
+
+    chrome.storage.sync.get(['state'], function (result) {
+        isOn = result.state !== 'off';
+        chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {message: "set_state", state: isOn});
+        })
+        changeView(isOn)
+    });
+
+
+    button.addEventListener('click', () => {
+        console.log('тык')
+        isOn = !isOn
+        changeView(isOn)
+        chrome.storage.sync.set({state: isOn ? 'on' : 'off'});
+        chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {message: "set_state", state: isOn});
+        })
+    }, false)
+
+
+    const langs = [['ru', 'Russian'], ['en', 'English'], ['fr', 'French'], ['de', 'Deutsch'], ['uk', 'Ukrainian'], ['it', 'Italian'], ['cs', 'Czech'], ['pl', 'Polish']]
     chrome.storage.sync.get(['lang'], function (result) {
         console.log('result is ', JSON.stringify(result))
-        selector.value = result.lang
+        const langIndex = langs.flat().indexOf(result.lang) / 2
+        selector.value = langs[langIndex][1]
     });
     for (let i = 0; i < langs.length; i++) {
         let opt = document.createElement('option');
-        opt.value = langs[i];
-        opt.innerHTML = langs[i];
+        opt.value = langs[i][1]
+        opt.innerHTML = langs[i][1];
         selector.appendChild(opt);
     }
     selector.addEventListener('change', () => {
         chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, {message: "set_lang", elem: selector.value});
-            chrome.storage.sync.set({lang: selector.value});
+            chrome.tabs.sendMessage(tabs[0].id, {message: "set_lang", elem: langs[selector.selectedIndex][0]});
+            chrome.storage.sync.set({lang: langs[selector.selectedIndex][0]});
         })
     }, false)
 }, false)
